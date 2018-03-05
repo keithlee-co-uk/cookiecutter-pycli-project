03a17138e13afd7db2c538a4fbab44617828b1d4
Keith Lee <keith@debian9>
2018-03-05 13:55:45 +0000

    Initial project files

---
+++ {{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_name}}.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+{{cookiecutter.project_name}} tests
+
+{{cookiecutter.project_short_description}}
+
+@author: {{cookiecutter.full_name}}
+"""
+
+
+
